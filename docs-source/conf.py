ca30ce87abb8747e13ffa7c468d4e57a59ba59eb
Viktor Kertesz <vkertesz2@gmail.com>
2021-11-22 21:38:10 +0100

    docs overhaul to support gh-pages

--- docs-source/conf.py
+++ docs-source/conf.py
@@ -16,7 +16,7 @@ import pkg_resources
 import toml
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../docs'))
 
 toml_dict = toml.load("../../pyproject.toml")
 # -- Project information -----------------------------------------------------
